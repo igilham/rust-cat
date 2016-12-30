b3cea859484bc7a78fecda9cfcdb0bda9cc6ef52
Ian Gilham <ian.gilham@bbc.co.uk>
2016-12-30 16:24:30 +0000

    refactored to reduce duplication

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs::File;
+use std::fmt::Display;
 use std::io;
 use std::io::BufReader;
 use std::io::prelude::*;
@@ -8,53 +9,44 @@ use std::path::Path;
 const STDIN: &'static str = "-";
 const BUFFER_SIZE: usize = 4096;
 
+enum Method {
+    File,
+    Line,
+}
+
 fn main() {
     // skip the first element as it is the program name
     let args = env::args();
     if args.len() == 1 {
-        cat_stdin();
+        cat(io::stdin(), "stdin", Method::Line);
     } else {
         for arg in args.skip(1) {
             if arg.eq(STDIN) {
-                cat_stdin();
+                cat(io::stdin(), "stdin", Method::Line);
             } else {
                 let path = Path::new(&arg);
-                cat(&path);
+                let display = path.display();
+                let file = File::open(path)
+                    .expect(format!("cat: failed to open: {}", display).as_str());
+                cat(file, display, Method::File);
             }
         }
     }
 }
 
-fn cat_stdin() {
-    let stdin = io::stdin();
-    let mut s = String::new();
-    loop {
-        match stdin.read_line(&mut s) {
-            Err(why) => panic!("cat: failed to read from stdin: {}", why),
-            Ok(n) => if n == 0 {
-                break;
-            } else {
-                print!("{}", s);
-                s.clear();
-            },
-        };
-    }
-}
-
-fn cat(path: &Path) {
-    let display = path.display();
-    let file = File::open(path)
-       .expect(format!("cat: failed to open: {}", display).as_str());
-    
-    let mut reader = BufReader::with_capacity(BUFFER_SIZE, file);
+fn cat<R, N>(input: R, name: N, m: Method) where R: Read, N: Display {
+    let mut reader = BufReader::with_capacity(BUFFER_SIZE, input);
     loop {
         let mut s = String::new();
-        let n = reader.read_to_string(&mut s)
-            .expect(format!("cat: failed to open {}", display).as_str());
+        let n = match m {
+            Method::Line => reader.read_line(&mut s),
+            Method::File => reader.read_to_string(&mut s),
+        }.expect(format!("cat: failed to open {}", name).as_str());
         if n > 0 {
             print!("{}", s);
         } else {
             break;
         }
     }
+    io::stdout().flush().unwrap();
 }
